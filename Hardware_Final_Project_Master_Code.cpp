9f6392ebe30f18190b476f4bf68c0af3c8613af8
Ryan Hoang <ryanbh1991@gmail.com>
2023-04-28 17:58:19 -0400

    Minor Changes Co-authored-by: Elijah Gibson <semphorin@users.noreply.github.com>

--- Hardware_Final_Project_Master_Code.cpp
+++ Hardware_Final_Project_Master_Code.cpp
@@ -1,13 +1,14 @@
 #include <SPI.h>
+#include <Tiny4kOLED.h>
 
 int slaveSelect;
 
 
-int detectPin;
+int detectPin = 3;
 bool detectFlag;
 bool prev;
 
-int weightLoss; 
+int weightLoss = 0; 
 
 int sendPin = 2; 
 
@@ -24,12 +25,25 @@ void setup()
   digitalWrite(slaveSelect, HIGH);
 
   attachInterrupt(digitalPinToInterrupt(sendPin),sendWeight,RISING);
-  
+  oled.begin();
+  oled.setFont(FONT8X16);
+  oled.clear();
+  oled.switchRenderFrame();
+  oled.clear();
+  oled.switchRenderFrame();
 }
 
 void loop() 
 { 
   detectSensor();
+  oled.setCursor(0,0);
+  oled.print("Today's ");
+  oled.setCursor(0,2);
+  oled.print("WLoss (lbs): ");
+  oled.print(weightLoss);
+  oled.on();
+  delay(500);
+
 
 }
 void detectSensor()
@@ -43,8 +57,9 @@ void detectSensor()
 }
 void sendWeight()
 {
+  weightLoss = 0;
   digitalWrite(slaveSelect, LOW);
-  SPI.transfer(count);
+  SPI.transfer(weightLoss);
   delay(500);
   digitalWrite(slaveSelect, HIGH);  
   delay(500);
